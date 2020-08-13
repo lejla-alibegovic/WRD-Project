25038739e5be61096fe3c410c991a10b432b08ac
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-13 19:25:24 +0200

    Update script.js

--- wrd project/script/script.js
+++ wrd project/script/script.js
@@ -19,32 +19,88 @@ $("#form").validate({
         },
         telephone: {
             regex: /^\+[0-9]{3}\-[0-9]{2}\-[0-9]{3}\-[0-9]{3}$/
+        },
+        activity:{
+            required:true
+        },
+        persons:{
+            required:true
         }
+
     }
 })
-
-function isValid(){
-    if (!$("#form").valid()) {  
-        alert('Please check your input');
-        return;
-    }
-    else{
-    alert("Thank you for your reservation");
-    window.scrollTo(0,0);
-    }
-}
-
-
 
 /* Calculate final cost */
 
 function finalCost(){
     var activity_type=document.getElementById("activity-type").value;
     var personsNumb=document.getElementById("person_number").value;
-    var total= (parseInt(activity_type)*parseInt(personsNumb))
+    var total= (parseInt(activity_type)*parseInt(personsNumb));
     document.getElementById("result").innerHTML=total;
 }
 
 
 
+/*Mobile menu */
+
+$(document).ready(function(){
+    $('.open-button').click(function(){
+        $(this).toggleClass('open');
+    });
+    $('.open-button').click(function(e){
+        e.preventDefault();
+        $('.nav-menu').toggleClass('open');
+    });
+});
+
+$(document).ready(function(){
+    $('.nav-menu a').click(function(){
+        $('.nav-menu').toggleClass('open');
+        $('.open-button').removeClass('open');
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/*Local storage*/
+
+function getIndex(){
+    var index=localStorage.getItem('index');
+    if(index==undefined){
+        return 0;
+    }
+    return index;
+}
+
+function Save(){
+    if(!$("#form").valid()){
+        alert('Please check your input!');
+        return;
+    }
+    var index=getIndex();
+    
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var telephone=document.getElementById('telephone').value;
+
+    localStorage.setItem('name'+index,name);
+    localStorage.setItem('email'+index,email);
+    localStorage.setItem('telephone'+index,telephone);
+
+   
+    index++;
+    localStorage.setItem('index',index);
+    alert('Successfull input.');
+
+    $("#form").reset();
+    
+}
 
